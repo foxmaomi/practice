c81c6b50bad4283f47de589fd6468a2a1313e952
666 <2868283704@qq.com>
2019-12-09 22:48:45 +0800

    test2019-12-09

---
+++ C++/tets2019-12-09/test.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+//#include<vld.h>
+using namespace std;
+
+void main()
+{
+    return;
+}
+
+
+
+/*void fun(Test x)
+{
+    int value = x.
+}
+
+void main()
+{
+    Test t(10);
+    fun(t);
+}
+/*class Date 
+{
+public:
+    Date(int year = 1900, int month = 1, int day = 1);
+    Date(const Date& d);
+    Date& operator=(const Date& d);
+    Date operator+(int days);
+    Date operator-(int days);
+    int operator-(const Date& d);
+    Date& operator++();
+    Date operator++(int);
+    Date& operator--();
+    Date operator--(int);
+    bool operator>(const Date& d)const;
+    bool operator>=(const Date& d)const;
+    bool operator<(const Date& d)const;
+    bool operator<=(const Date& d)const;
+    bool operator==(const Date& d)const;
+    bool operator!=(const Date& d)const;
+private:
+    int _year;
+    int _month;
+    int _day;
+};
+Date::Date(int year, int month, int day)
+{
+	_year = year;
+	_month = month;
+	_day = day;
+}
+int main()
+{
+    
+    Date dt(2019, 12, 9);
+    return 0;
+}
+²ÌÀÕ¹«Ê½
+*/
+

ec3e55f996ffccf64bf8df6f5893cab851f73af8
666 <2868283704@qq.com>
2020-03-02 23:55:47 +0800

    01

---
+++ 每日一题/三月份/test01/test.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+/*
+int main()
+{
+    int x = 0;
+    int y = 0;
+
+    for (x = 0, y = 0; (y = 123) && (x < 4); x++)
+    {
+        printf("11111111\n");
+    }
+}
+/*
+int main()
+{
+    printf("%s\n%5.3s\n", "computer", "computer");
+    return 0;
+}
+/*
+int main() {
+    //freopen("1.in","r",stdin);
+    int n, ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int x; 
+            scanf("%d", &x);
+            ans += x;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+*/

6477fbb7ac35904c92eacac6a92ac3d4b4368714
666 <2868283704@qq.com>
2020-03-08 22:01:09 +0800

    补充

--- 每日一题/三月份/test3_06/test.cpp
+++ 每日一题/三月份/test3_06/test.cpp
@@ -1,10 +1,123 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<stdio.h>
+#include<string>
 using namespace std;
+#define MAX 100000
+int main()
+{
+	int ar[MAX] = { 0 };
+	int len = 0;
+	cin >> len;
+	
+	int max = 0;
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		cin >> ar[i];
+	}
+	i = 0;
+	while (i<len)
+	{
+		int count = 0;
+		while (ar[i] < 0)
+			i++;
+		int left = i;
+		while (ar[i] > 0)
+			i++;
+		int right = i;
+		while (left <= right)
+		{
+			count += ar[left];
+			left++;
+		}
+		if(max>count)
+		{ }
+		else
+		{
+			max = count;
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
 
 
 
+
+/*
+int main()
+{
+	string s;
+	cin >> s;
+	int begin = 0;
+	int end = s.size() - 1;
+	while (begin++ < end--)
+	{
+		if (begin < end )
+		{
+			if ((s[begin] == s[end]))
+			{
+			}
+			else
+				break;
+		}
+		else
+			break;
+	}
+	if (s[begin] != s[end])
+	{
+		cout << "error" << endl;
+	}
+	else
+	{
+		cout << "yes" << endl;
+	}
+}
+
+
+/*
+void Insert(std::string s1,std::string s2,int count)
+{
+	auto x = s2.size();
+
+}
+
+int main()
+{
+	string s1;
+	string s2;
+	string s3;
+	cin >> s1;
+	cin >> s2;
+	int count = 0;
+	s1.reserve(100);
+	s1.reserve(100);
+	string::iterator right = s1.begin();
+	while (right++ != s1.end())
+	{
+		string s4 = s1;
+		string s5 = s2;
+		Insert(s4, s5, count);
+		count++;
+		int begin = 0;
+		int end = s4.size() - 1;
+		while (begin++ <= end--)
+		{
+			if (begin < end)
+			{
+				if()
+			}
+		}
+	}
+
+
+}
+
+
+
+
+/*
 int main()
 {
 	int a;

ecf8bebef3e7118dc9ee048651e895c1fd5d37cc
666 <2868283704@qq.com>
2020-03-08 22:02:57 +0800

    补充

--- 每日一题/三月份/test04/test04/test.cpp
+++ 每日一题/三月份/test04/test04/test.cpp
@@ -3,6 +3,31 @@
 #include<vector>
 using namespace std;
 
+int main(void)
+{
+	vector<int>array;
+	array.push_back(100);
+	array.push_back(300);
+	array.push_back(300);
+	array.push_back(300);
+	array.push_back(300);
+	array.push_back(500);
+	vector<int>::iterator itor;
+	for (itor = array.begin(); itor != array.end(); itor++)
+	{
+		if (*itor == 300)
+		{
+			itor = array.erase(itor);
+		}
+	}
+	for (itor = array.begin(); itor != array.end(); itor++)
+	{
+		cout << *itor << "";
+	}
+	return 0;
+}
+
+/*
 int func()
 {
 	int i, j, k = 0;

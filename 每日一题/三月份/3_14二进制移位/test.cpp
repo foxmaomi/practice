43f050cf19680cbe1bf2e9be98026cf15fc3f2ed
666 <2868283704@qq.com>
2020-04-06 21:15:20 +0800

    14

--- 每日一题/三月份/3_14二进制移位/test.cpp
+++ 每日一题/三月份/3_14二进制移位/test.cpp
@@ -1,10 +1,74 @@
-#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+#define STACK_INIT_SIZE 100;
+#define STACKINCREMEMT 10;
+typedef struct {
+    SElemType* base;
+    SElemType* top;
+    int stacksize;
+} sqstack;
+
+status Initstack(sqstack& s) {
+    s.base = (SElemType*)malloc(STACK_INIT_SIZE * sizeof(SElemType));
+    if (!s.base) exit(OVERFLOW);
+    s.stacksize = STACK_INIT_SIZE;
+    s.top = s.base;
+    return OK;
+}  //;
+
+status Push(sqstack& s, SElemType e) {
+    if (s.top - s.base >= s.stacksize) {
+        s.base = (SElemType*)realloc(s.base, sizeof(SElemType) * (s.stacksize + STACKINCREMEMT));
+        if (!s.base) exit(OVERFLOW);
+        s.top = s.base + s.stacksize;
+        s.stacksize += STACKINCREMEMT;
+    }
+    *(s.top) = e;
+    s.top++;
+    return OK;
+};
+
+status Pop(sqstack& s, SElemType& e) {
+    if (s.top == s.base) return ERROR;
+    e = *(--s.top)
+        return OK;
+}
+
+void conversion() {
+    int x;
+    status Initstack(s);  //s  ?
+    printf("请输入转换的十进制数");
+    scanf("%u",&N);//缺少"
+        printf("请输入转换为多少进制");
+    scanf("%d",&x);//缺少"
+        while (N) {
+            status Push(s, N % x);
+            N = N / x;
+        }
+    while (!stackEmpty(s)) {
+        status Pop(s, e);
+        printf("%d", e);
+    }
+}
+
+void main()
+{
+    void conversion();
+}
+
+
+
+
+
+
+
+
+/*#include<iostream>
 #include<algorithm>
 using namespace std;
-
-
-
-
 /*
 int main()
 {

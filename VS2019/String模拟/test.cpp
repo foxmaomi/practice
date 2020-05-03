26c1b1a7e76716b3830e3c5ccf36c0ecd42db930
666 <2868283704@qq.com>
2020-05-03 21:11:37 +0800

    3

--- VS2019/String模拟/test.cpp
+++ VS2019/String模拟/test.cpp
@@ -2,10 +2,19 @@
 #include<iostream>
 #include<string>
 //#include<vld.h>
+#include<vector>
 #include<assert.h>
-
+#include <string_view>
+//using namespace std::basic_string_view;
+//using namespace std::basic_string;
 using namespace std;
 
+
+
+
+
+
+/*
 namespace bit
 {
 	class string
@@ -95,28 +104,128 @@ namespace bit
 		}
 		void resize(size_t newSize, char c = '\0')
 		{
+			if (newSize > m_size)
+			{
+				if (newSize > m_capacity)
+				{
+					reserve(newSize * 2);
+				}
+				memset(m_str + m_size, c, newSize - m_size);
+			}
+			m_size = newSize;
+			m_str[m_size] = '\0';
 
 		}
 		const char& operator[](size_t index)const;
-		bool operator<(const string& s);
-		bool operator<=(const string& s);
-		bool operator>(const string& s);
-		bool operator>=(const string& s);
-		bool operator==(const string& s);
-		bool operator!=(const string& s);
+		bool operator<(const string& s)
+		{
+			return(strcmp(m_str, s.m_str) < 0);
+		}
+		bool operator<=(const string& s)
+		{
+			return !(*this > s);
+		}
+		bool operator>(const string& s)
+		{
+			return(strcmp(m_str, s.m_str) > 0);
+		}
+		bool operator>=(const string& s)
+		{
+			return !(*this < s);
+		}
+
+		bool operator==(const string& s)
+		{
+			return (strcmp(m_str, s.m_str) == 0);
+		}
+		bool operator!=(const string& s)
+		{
+			return !(*this == s);
+		}
 		//返回c在string中第一次出现的位置
-		size_t find(char c, size_t pos = 0)const;
+		size_t find(char c, size_t pos = 0)const
+		{
+			for (int i = pos; i < m_size; ++i)
+			{
+				if (m_str[i] == c)
+					return i;
+			}
+			return std::string::npos;
+		}
 		//返回字串s在string中第一次出现的位置
-		size_t find(const char* s, size_t pos = 0)const;
+		size_t find(const char* s, size_t pos = 0)const
+		{
+			size_t i = pos, j = 0;
+			size_t s_len = strlen(s);
+			while (i < m_size && j < s_len)
+			{
+				if (m_str[i] == s[j])
+				{
+					i++;
+					j++;
+				}
+				else
+				{
+					i = i - j + 1;
+					j = 0;
+				}
+			}
+			if (j >= s_len)
+			{
+				return i - j;
+			}
+			return std::string::npos;
+		}
 		//在pos位置上插入字符c/字符串str，并返回该字符的位置
-		string& insert(size_t pos, char c);
-		string& insert(size_t pos, const char* str);
+		string& insert(size_t pos, char c)
+		{
+			if (m_size + 1 > m_capacity)
+			{
+				reserve((m_size + 1) * 2);
+			}
+			for (size_t i = m_size; i > pos; i--)
+			{
+				m_str[i] = m_str[i - 1];
+			}
+			m_str[pos] = c;
+			m_size++;
+			return *this;
+		}
+		string& insert(size_t pos, const char* str)
+		{
+			int a = 0;
+			size_t len = strlen(str);
+			if (m_size + len > m_capacity)
+			{
+				reserve((m_size + len) * 2);
+			}
+			for (size_t i = m_size; i > m_size + len; i--)
+			{
+				m_str[i] = m_str[i - 1];
+			}
+			for (size_t i = pos; i <= pos + len; i++)
+			{
+				m_str[i] = str[a];
+				a++;
+			}
+			return *this;
+		}
 		//删除pos位置上的元素，并返回该元素的下一个位置
-		string& erase(size_t pos, size_t len);
+		string& erase(size_t pos, size_t len)
+		{
+			memcpy(m_str + pos, m_str + pos + len, m_size - pos - len + 1);
+			m_size -= len;
+			return *this;
+		}
 	public:
 		char& operator[](int i)
 		{
 			assert(i >= 0 && i< m_size);
+			return m_str[i];
+		}
+		const char& operator[](size_t i)const
+		{
+			assert(i >= 0 && i < m_size);
 			return m_str[i];
 		}
 	public:
@@ -131,6 +240,13 @@ namespace bit
 				m_str = new_str;
 			}
 		}
+		protected:
+			static void _swap(string& s1, string & s2)
+			{
+				std::swap(s1.m_str, s2.m_str);
+				std::swap(s1.m_capacity, s2.m_capacity);
+				std::swap(s1.m_size, s2.m_size);
+			}
 	private:
 		char* m_str;
 		size_t m_capacity;
@@ -138,9 +254,46 @@ namespace bit
 	};
 	ostream& operator<<(ostream& out, const string& s)
 	{
-		out << s.m_str;
+		for (int i = 0; i < s.m_size; ++i)
+		{
+			out << s.m_str[i];
+		}
+		//out << s.m_str;
 		return out;
 	}
+	istream& operator>>(istream& _cin, bit::string& s)
+	{
+		char* str = (char*)malloc(sizeof(char) * 10);
+		char* buf = str;
+		size_t count = 1;
+		//跳过字符串起始位置的空格或者换行
+		while ((*buf = getchar()) == ' ' || (*buf == '\n'));
+		for (;;)
+		{
+			if (*buf == '\n' || *buf == ' ')
+			{
+				*buf = '\0';
+				break;
+			}
+			else if (count % 10 == 0)
+			{
+				str = (char*)realloc(str, count * 2);
+				//重新定位buf的位置
+				buf = str + count;
+				count++;
+				*buf = getchar();
+			}
+			else
+			{
+				*++buf = getchar();
+				count++;
+			}
+		}
+		s.m_str = str;
+		s.m_capacity = s.m_size = count;
+		return _cin;
+	}
+
 }
 void main()
 {
